256045cdf558ca3f493f9616938a429b6fec56fc
Patryk <p.stronski13@gmail.com>
2019-01-21 20:43:34 +0100

    sth

--- list6/getVoivs.php
+++ list6/getVoivs.php
@@ -2,6 +2,7 @@
     $dbhost = "127.0.0.1:3306";
     $dbuser = "root";
     $con = mysqli_connect($dbhost, $dbuser);
+    mysqli_set_charset($con,"utf8");
     if($con) {
         $db = mysqli_select_db($con,'fedokredo');
         $res = mysqli_query($con,'SELECT * FROM wojewodztwa;');
